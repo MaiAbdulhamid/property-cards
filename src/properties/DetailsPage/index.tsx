2789122209e603533e3d89c1a748de054d516530
maiAbdulhameed <mai.abdelhameed@rowaad.net>
2023-11-01 22:50:17 +0200

    Initial

--- src/properties/DetailsPage/index.tsx
+++ src/properties/DetailsPage/index.tsx
@@ -18,18 +18,24 @@ const DetailsPage = () => {
 
   return (
     <div>
-      <img src={data[0].imgSrc} />
-      <p>#{data[0].id}</p>
-      {data[0].name && (
-        <p className="text-gray-700 text-base">Name: {data[0].name}</p>
+      <img
+        src={data[0]?.imgSrc}
+        style={{
+          borderRadius: "10px",
+          margin: "1rem 0"
+        }}
+      />
+      <p>#{data[0]?.id}</p>
+      {data[0]?.name && (
+        <p className="text-gray-700 text-base">Name: {data[0]?.name}</p>
       )}
-      {data[0].type && (
-        <p className="text-gray-700 text-base">Type: {data[0].type}</p>
+      {data[0]?.type && (
+        <p className="text-gray-700 text-base">Type: {data[0]?.type}</p>
       )}
-      <p className="text-gray-700 text-base">Address: {data[0].address}</p>
-      <p className="text-gray-700 text-base">Lat: {data[0].latitude}</p>
-      <p className="text-gray-700 text-base">Lang: {data[0].longitude}</p>
-      <p className="text-gray-700 text-base">Area: {data[0].area}</p>
+      <p className="text-gray-700 text-base">Address: {data[0]?.address}</p>
+      <p className="text-gray-700 text-base">Lat: {data[0]?.latitude}</p>
+      <p className="text-gray-700 text-base">Lang: {data[0]?.longitude}</p>
+      <p className="text-gray-700 text-base">Area: {data[0]?.area}</p>
     </div>
   );
 };
