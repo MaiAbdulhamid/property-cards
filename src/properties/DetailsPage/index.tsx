77bf8ca933d87554d40d97cc22d30f00adac6751
maiAbdulhameed <mai.abdelhameed@rowaad.net>
2023-11-01 22:41:56 +0200

    Initial

--- src/properties/DetailsPage/index.tsx
+++ src/properties/DetailsPage/index.tsx
@@ -1,29 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { fetchWrapper } from '../../helpers';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchWrapper } from "../../helpers";
 
 const DetailsPage = () => {
-  const [data, setData] = useState<any>({})
+  const [data, setData] = useState<any>({});
   const params = useParams();
-  
+
   const fetchProperty = async () => {
-    const response = await fetchWrapper.get(`${process.env.REACT_APP_API_URL}/properties/${params.id}`);
-    setData(response)
-  }
+    const response = await fetchWrapper.get(
+      `${process.env.REACT_APP_API_URL}/properties?id=${params.id}`
+    );
+    setData(response);
+  };
   useEffect(() => {
-    fetchProperty()
+    fetchProperty();
   }, []);
 
   return (
     <div>
-      <img src={data.imgSrc} />
-      <p>#{data.id}</p>
-      <p className="text-gray-700 text-base">Address: {data.address}</p>
-      <p className="text-gray-700 text-base">Lat: {data.latitude}</p>
-      <p className="text-gray-700 text-base">Lang: {data.longitude}</p>
-      <p className="text-gray-700 text-base">Area: {data.area}</p>
+      <img src={data[0].imgSrc} />
+      <p>#{data[0].id}</p>
+      {data[0].name && (
+        <p className="text-gray-700 text-base">Name: {data[0].name}</p>
+      )}
+      {data[0].type && (
+        <p className="text-gray-700 text-base">Type: {data[0].type}</p>
+      )}
+      <p className="text-gray-700 text-base">Address: {data[0].address}</p>
+      <p className="text-gray-700 text-base">Lat: {data[0].latitude}</p>
+      <p className="text-gray-700 text-base">Lang: {data[0].longitude}</p>
+      <p className="text-gray-700 text-base">Area: {data[0].area}</p>
     </div>
-  )
-}
+  );
+};
 
-export default DetailsPage
+export default DetailsPage;
--- src/properties/Property.tsx
+++ src/properties/Property.tsx
@@ -1,37 +1,43 @@
+import { Link } from "react-router-dom";
+
 function Property({ property }: any) {
   return (
     <li className="p-3 m-3 border-none flex justify-between">
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        {property.hasImage && (
-          <img
-            className="w-full"
-            src={property?.imgSrc}
-            alt="Sunset in the mountains"
-          />
-        )}
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">{property.name}</div>
-          <p className="text-gray-700 text-base">{property.address}</p>
-          {property.type && (
-            <p className="text-gray-700 text-base">Type: {property.type}</p>
+      <Link to={`/${property.id}`}>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+          {property.hasImage && (
+            <img
+              className="w-full"
+              src={property?.imgSrc}
+              alt="Sunset in the mountains"
+            />
           )}
-          <div className="font-bold text-xl mb-2">{property.price}</div>
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">{property.name}</div>
+            <p className="text-gray-700 text-base">{property.address}</p>
+            {property.type && 
+              <p className="text-gray-700 text-base">
+                Type: {property.type}
+              </p>
+            }
+            <div className="font-bold text-xl mb-2">{property.price}</div>
+          </div>
+          <div className="px-6 py-4">
+            {property.beds && (
+              <span className="flex w-fit bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                {property.beds}
+                <img src="https://hirondelle-properties.com/assets/bed-rooms-aed6fad5.svg" />
+              </span>
+            )}
+            {property.paths && (
+              <span className="flex w-fit bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+                {property.paths}
+                <img src="https://hirondelle-properties.com/assets/bath-rooms-afcecd61.svg" />
+              </span>
+            )}
+          </div>
         </div>
-        <div className="px-6 py-4">
-          {property.beds && (
-            <span className="flex w-fit bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-              {property.beds}
-              <img src="https://hirondelle-properties.com/assets/bed-rooms-aed6fad5.svg" />
-            </span>
-          )}
-          {property.paths && (
-            <span className="flex w-fit bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-              {property.paths}
-              <img src="https://hirondelle-properties.com/assets/bath-rooms-afcecd61.svg" />
-            </span>
-          )}
-        </div>
-      </div>
+      </Link>
     </li>
   );
 }
